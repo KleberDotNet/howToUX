37e2ed5f7294b8cebe46ac0adee7b783b09b4b6b
KleberDotNet <kleberkelvyn@gmail.com>
2021-10-21 17:37:16 -0300

    Adding popovers, popper.js

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ const $button = document.querySelector('#dark-mode-switch')
 $button.addEventListener('click', function() {
     $html.classList.toggle('dark-mode')
 })
+
+//Initialize Popovers
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
